e90645e4df44a1e8b8dbc4f6a8037721dd52be35
Wannes <vandeputte.wannes@gmail.com>
2024-11-27 23:27:40 +0100

    Fix rocket-react connection

--- gui/backend/src/main.rs
+++ gui/backend/src/main.rs
@@ -6,7 +6,9 @@ use rocket::State;
 use rocket::form::Form;
 use rocket::fs::{FileServer, relative};
 use rocket::response::{Flash, Redirect};
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize, json::Json};
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
 
 use rocket_dyn_templates::{Template, context};
 
@@ -15,24 +17,21 @@ use lbf::lbf_run::solve_json;
 type SvgFiles = Mutex<Vec<String>>; // Define a type alias for shared state.
 
 
-#[derive(Debug, FromForm)]
-pub struct Json {
+#[derive(Deserialize, Serialize)]
+pub struct InputData {
     pub json_str: String,
 }
 
-#[get("/")]
-fn index() -> Template {
-    Template::render("index", context! {field: "value"})
-}
 
-#[post("/json", data = "<form_data>")]
-async fn json(form_data: Form<Json>, svg_state: &State<SvgFiles>) -> Flash<Redirect> {
-    let json = form_data.into_inner();
+
+#[post("/json", format = "json", data = "<input_data>")]
+async fn json(input_data: Json<InputData>, svg_state: &State<SvgFiles>) -> Flash<Redirect> {
+    let json = input_data.into_inner();
     if json.json_str.is_empty() {
         return Flash::error(Redirect::to("/"), "JSON cannot be empty.")
     }
 
-    let svg_files = solve_json(json.json_str.clone(), "./static/assets/solutions/".to_string());
+    let svg_files = solve_json(json.json_str.clone(), "./static/solutions/".to_string());
     if svg_files.is_empty() {
         return Flash::error(Redirect::to("/"), "No solution found.")
     } else {
@@ -51,17 +50,33 @@ fn sol(svg_state: &State<SvgFiles>) -> Template {
     let adjusted_path_json = path.replace(".svg", ".json")
                                     .replace("/static", "")
                                     .replace("_0", "");
-    // let adjusted_path_svg = "/assets/solutions/sol_web_0.svg";
+    // let adjusted_path_svg = "/solutions/sol_web_0.svg";
     println!("{}", adjusted_path_svg);
 
     Template::render("solution", context! {path_svg: adjusted_path_svg, path_json: adjusted_path_json})
 }
 
+
 #[launch]
 fn rocket() -> _ {
+    // Configure CORS options
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::some_exact(&["http://localhost:3000"]),
+        allowed_methods: vec![Method::Get, Method::Post, Method::Options]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::some(&["Content-Type"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("CORS configuration failed");
+
     rocket::build()
         .manage(SvgFiles::default()) // Initialize shared state.
-        .mount("/", routes![index, json, sol])
+        .mount("/", routes![json, sol])
         .mount("/", FileServer::from(relative!("./static")))
         .attach(Template::fairing())
+        .attach(cors)
 }
