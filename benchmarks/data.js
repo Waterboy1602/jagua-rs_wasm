4599ec0e3b1054c23e4e65cdcb0f8f350b1f372e
Waterboy1602 <vandeputte.wannes@gmail.com>
2025-08-21 11:36:37 +0000

    auto deploy docs

---
+++ benchmarks/data.js
@@ -0,0 +1,554 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755769572742,
+  "repoUrl": "https://github.com/Waterboy1602/jagua-rs_wasm",
+  "entries": {
+    "Performance Tracker": [
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "4d129dd0a94afbea5d538418615c91873ea273fb",
+          "message": "Fix workspace cargo.toml",
+          "timestamp": "2025-06-17T14:18:53+02:00",
+          "tree_id": "8295dd8ae82c9ed64377f7bf56cf3277b72e7a3f",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/4d129dd0a94afbea5d538418615c91873ea273fb"
+        },
+        "date": 1750162905076,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2556274,
+            "range": "± 129621",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 3003378,
+            "range": "± 201709",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 3008591,
+            "range": "± 213962",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2270792,
+            "range": "± 16392",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4110074,
+            "range": "± 25535",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 8328169,
+            "range": "± 71802",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 267908,
+            "range": "± 6365",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 235415,
+            "range": "± 5231",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 225988,
+            "range": "± 10637",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "2985442b967536b51706e39b32377d6924560a03",
+          "message": "Merge remote-tracking branch 'upstream/main'",
+          "timestamp": "2025-07-09T13:40:21+02:00",
+          "tree_id": "30f3aa31bec4e0dbbf9b65470beba6224a812f62",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/2985442b967536b51706e39b32377d6924560a03"
+        },
+        "date": 1752061487944,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2275634,
+            "range": "± 107149",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2629373,
+            "range": "± 144605",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2638231,
+            "range": "± 148355",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2355939,
+            "range": "± 22178",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4217807,
+            "range": "± 19582",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 8109818,
+            "range": "± 43494",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 244610,
+            "range": "± 6369",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 220735,
+            "range": "± 4257",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 218473,
+            "range": "± 3130",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "86820711c22aa8dd2c18adef9298b88c771088d3",
+          "message": "Move web_time definition to lib.rs",
+          "timestamp": "2025-08-05T14:06:24+02:00",
+          "tree_id": "4bb36d682b17f84183e5305228d6f213286391af",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/86820711c22aa8dd2c18adef9298b88c771088d3"
+        },
+        "date": 1754395798884,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2272273,
+            "range": "± 105907",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2631534,
+            "range": "± 143311",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2632485,
+            "range": "± 142180",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2419693,
+            "range": "± 7682",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4232156,
+            "range": "± 14980",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 8267127,
+            "range": "± 54019",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 246410,
+            "range": "± 6292",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 222604,
+            "range": "± 4270",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 219473,
+            "range": "± 3589",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "d8f5de4e8e3bc96b225ab37f9628afa3753a7a5f",
+          "message": "Merge branch 'main' of github.com:JeroenGar/jagua-rs",
+          "timestamp": "2025-08-05T14:10:06+02:00",
+          "tree_id": "bfda0a3cfe9418c54e289c88e8094fabc3c7220f",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/d8f5de4e8e3bc96b225ab37f9628afa3753a7a5f"
+        },
+        "date": 1754395988077,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2253291,
+            "range": "± 107515",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2600846,
+            "range": "± 136687",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2594942,
+            "range": "± 137451",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2265833,
+            "range": "± 24884",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4035310,
+            "range": "± 38733",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 7755715,
+            "range": "± 49848",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 243170,
+            "range": "± 6226",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 222158,
+            "range": "± 4140",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 219095,
+            "range": "± 3042",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "01c13d5e31a8f6b09b23b567f591a272f5b7c638",
+          "message": "Revert \"Merge branch 'main' of github.com:JeroenGar/jagua-rs\"\n\nThis reverts commit d8f5de4e8e3bc96b225ab37f9628afa3753a7a5f, reversing\nchanges made to 86820711c22aa8dd2c18adef9298b88c771088d3.",
+          "timestamp": "2025-08-05T16:40:54+02:00",
+          "tree_id": "4bb36d682b17f84183e5305228d6f213286391af",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/01c13d5e31a8f6b09b23b567f591a272f5b7c638"
+        },
+        "date": 1754405047472,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2274576,
+            "range": "± 114841",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2636705,
+            "range": "± 142876",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2642048,
+            "range": "± 144979",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2391341,
+            "range": "± 34203",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4279792,
+            "range": "± 15490",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 8568634,
+            "range": "± 58623",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 247198,
+            "range": "± 5874",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 224022,
+            "range": "± 4229",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 220668,
+            "range": "± 2924",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "0cc08a6bf4f5f78d55ae16252a2f4550ad8ee933",
+          "message": "Merge remote-tracking branch 'upstream/main'",
+          "timestamp": "2025-08-21T11:01:17+02:00",
+          "tree_id": "f9fe9f9895197ce760fbcee1eda31b8a642cbb57",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/0cc08a6bf4f5f78d55ae16252a2f4550ad8ee933"
+        },
+        "date": 1755767083960,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2261162,
+            "range": "± 111345",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2619383,
+            "range": "± 141684",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2621138,
+            "range": "± 159244",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2319627,
+            "range": "± 12972",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4194563,
+            "range": "± 20890",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 8108639,
+            "range": "± 53041",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 242932,
+            "range": "± 6233",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 218057,
+            "range": "± 4224",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 216851,
+            "range": "± 3081",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jeroen.gardeyn@hotmail.com",
+            "name": "Jeroen Gardeyn",
+            "username": "JeroenGar"
+          },
+          "committer": {
+            "email": "vandeputte.wannes@gmail.com",
+            "name": "Wannes",
+            "username": "Waterboy1602"
+          },
+          "distinct": true,
+          "id": "1f85019b84d8831227fd8b97aab6352b62efa2e5",
+          "message": "small changes",
+          "timestamp": "2025-08-21T11:40:10+02:00",
+          "tree_id": "a51ced06e2a21b43744d54c68fae72058e5040b5",
+          "url": "https://github.com/Waterboy1602/jagua-rs_wasm/commit/1f85019b84d8831227fd8b97aab6352b62efa2e5"
+        },
+        "date": 1755769561522,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cde_collect_1k/3",
+            "value": 2265165,
+            "range": "± 107752",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/4",
+            "value": 2611851,
+            "range": "± 141835",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_collect_1k/5",
+            "value": 2592278,
+            "range": "± 136153",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/3",
+            "value": 2327258,
+            "range": "± 18143",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/4",
+            "value": 4134695,
+            "range": "± 16209",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_update_1k/5",
+            "value": 7731908,
+            "range": "± 458625",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/3",
+            "value": 242637,
+            "range": "± 6409",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/4",
+            "value": 216756,
+            "range": "± 4307",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cde_detect_1k/5",
+            "value": 213263,
+            "range": "± 2943",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
